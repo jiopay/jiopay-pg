f600b09e89903c3e1a819cd91459bf83e762cbcf
Prashant Dwivedi <prashant1.dwivedi@ril,com>
2021-08-12 16:46:46 +0530

    first pg commit

---
+++ Jiopay-pg/Jiopay_pg.h
@@ -0,0 +1,18 @@
+//
+//  Jiopay_pg.h
+//  Jiopay-pg
+//
+//  Created by Prashant Dwivedi on 12/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jiopay_pg.
+FOUNDATION_EXPORT double Jiopay_pgVersionNumber;
+
+//! Project version string for Jiopay_pg.
+FOUNDATION_EXPORT const unsigned char Jiopay_pgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jiopay_pg/PublicHeader.h>
+
+
